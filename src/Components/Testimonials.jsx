1dc717e6209e2d88f0224a5f9fd222897fb7a8ba
Ivan <ivanpkhchan@gmail.com>
2019-02-09 09:41:01 -0500

    add testimonials section

---
+++ src/Components/Testimonials.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Testimonials.css';
+
+class Testimonials extends React.Component {
+  render() {
+    return <h1>Testimonials</h1>;
+  }
+}
+
+export default Testimonials;
